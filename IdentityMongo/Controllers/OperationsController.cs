5e8054b9c470fcf4b1d31e567c074199e67b0614
Lachezar Georgiev <49004684+glachezar@users.noreply.github.com>
2025-01-15 21:59:11 +0200

    Add OperationsController and user creation functionality
    Added OperationsController.cs with a new OperationsController class,
    including a constructor and two Create actions for user creation.
    Modified IdentityMongo.csproj to target .NET 8.0 framework.
    Added Create.cshtml view with a form for creating a new user.
    Updated _Layout.cshtml to include a navigation link to the Create action.

---
+++ IdentityMongo/Controllers/OperationsController.cs
@@ -0,0 +1,40 @@
+﻿namespace IdentityMongo.Controllers;
+using IdentityMongo.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+public class OperationsController : Controller
+{
+    private UserManager<ApplicationUser> userManager;
+
+    public OperationsController(UserManager<ApplicationUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public ViewResult Create() => View();
+
+    [HttpPost]
+    public async Task<IActionResult> Create(User user)
+    {
+        if (ModelState.IsValid)
+        {
+            ApplicationUser appUser = new ApplicationUser
+            {
+                UserName = user.Name,
+                Email = user.Email
+            };
+
+            IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+            if (result.Succeeded)
+                ViewBag.Message = "User Created Successfully";
+            else
+            {
+                foreach (IdentityError error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+            }
+        }
+        return View(user);
+    }
+}
